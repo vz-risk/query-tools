723b225e27bc8d5af9946f6f07015562676f6865
Nathan Buesgens <nathan@natb1.com>
2014-05-08 18:49:11 +0000

    adds an sqlite session manager (needs acceptance test)

--- query_tools/__init__.py
+++ query_tools/__init__.py
@@ -2,10 +2,12 @@ import criteria
 import csv_encoder
 import json_encoder
 import sqla
+import sqlite
 
 Criteria = criteria.Criteria
 Conjuction = criteria.Conjuction
 
 SQLAlchemy = sqla.SQLAlchemy
+SQLite = sqlite.SQLite
 JSONEncoder = json_encoder.JSONEncoder
 CSVEncoder = csv_encoder.CSVEncoder
---
+++ query_tools/sqlite.py
@@ -0,0 +1,11 @@
+#TODO: this is implemented as a sqla adaptor but could stand on its own to
+#get rid of the sqla requirement
+
+import sqla
+
+class SQLite(sqla.SQLAlchemy):
+
+    def __init__(self, sqla_metadata, aggregate_mappers, db_file_path):
+        engine_url = 'sqlite:///%s' % db_file_path
+        super(SQLite, self).__init__(
+            sqla_metadata, aggregate_mapper, engine_url)
