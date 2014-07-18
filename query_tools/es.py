1bb4daf05fde915d9f9240bc99e0d8bb160a9b0f
Nathan Buesgens <nathan@natb1.com>
2014-07-18 19:11:54 +0000

    es add_all uses official es module

--- query_tools/es.py
+++ query_tools/es.py
@@ -7,6 +7,7 @@ import subprocess
 import tempfile
 
 import elasticsearch
+import elasticsearch.helpers
 import mapping_tools
 
 import query_tools.json_encoder
@@ -14,22 +15,21 @@ import query_tools.json_encoder
 class ElasticSearch(object):
 
     def __init__(self, index, ModelType_to_type_name,
-                 ModelType_to_schema_mapper, host='localhost', port=9200):
+                 ModelType_to_schema_mapper, hosts=[{'host':'localhost'}]):
         self.index = index
         self.ModelType_to_type_name = ModelType_to_type_name
         self.ModelType_to_schema_mapper = ModelType_to_schema_mapper
         self.type_name_to_dict_mapper = dict(
             (type_name, mapping_tools.DictMapper(ModelType))
             for ModelType, type_name in ModelType_to_type_name.items())
-        self.host = host
-        self.port = port
+        self.hosts = hosts
 
     def make_session(self):
         session = ElasticSearchSession(
             self.index, self.ModelType_to_type_name,
             self.type_name_to_dict_mapper, 
             self.ModelType_to_schema_mapper,
-            self.host, self.port)
+            self.hosts)
         return session
 
     def setup(self):
@@ -40,13 +40,12 @@ class ElasticSearchSession(object):
     def __init__(self, index, ModelType_to_type_name,
                  type_name_to_dict_mapper,
                  ModelType_to_schema_mapper,
-                 host, port):
+                 hosts):
         self.index = index
         self.ModelType_to_type_name = ModelType_to_type_name
         self.type_name_to_dict_mapper = type_name_to_dict_mapper
         self.ModelType_to_schema_mapper = ModelType_to_schema_mapper
-        self.es = elasticsearch.Elasticsearch([{'host':host, 'port':port}])
-        self.host = host + ':' + str(port)
+        self.es = elasticsearch.Elasticsearch(hosts)
 
     def __enter__(self):
         return self
@@ -55,45 +54,21 @@ class ElasticSearchSession(object):
         pass #do nothing
 
     def add_all(self, domain_objects):
-        #TODO: handle multiple types
-        type_name = self.ModelType_to_type_name.values()[0]
-        dict_mapper = self.type_name_to_dict_mapper[type_name]
-        index_json = json.dumps({'index':{'_type':type_name}})
-        bulkfile = tempfile.NamedTemporaryFile()
-        MAX_FILE_SIZE = 104857600
-        SAFE_FILE_SIZE = MAX_FILE_SIZE/2
-        result = 0
-        for obj in domain_objects:
-            result += 1
-            bulkfile.write(index_json)
-            bulkfile.write('\n')
-            dict_obj = dict_mapper.map(obj)
-            bulkfile.write(json.dumps(
-                dict_obj, cls=ISODateTimeNoMicrosecond))
-            bulkfile.write('\n')
-            if os.stat(bulkfile.name).st_size > SAFE_FILE_SIZE:
-                self._flush_bulkfile(bulkfile)
-                bulkfile = tempfile.NamedTemporaryFile()
-        self._flush_bulkfile(bulkfile)
-        return result
-        
-    def _flush_bulkfile(self, bulkfile):
-        bulkfile.flush()
-        try:
-            result = json.loads(subprocess.check_output(
-                'curl -sS -XPOST {}/{}/_bulk --data-binary @{}'.format(
-                    self.host, self.index, bulkfile.name),
-                shell=True, stderr=subprocess.STDOUT))
-            if 'errors' in result and result['errors']:
-                print(result)
-            elif 'error' in result:
-                print(result['error'])
-        except subprocess.CalledProcessError as e:
-            print(e.output)
-        except Exception as e:
-            print(e)
-        bulkfile.close()
+        results = elasticsearch.helpers.bulk(
+           self.es, self._get_actions(domain_objects))
+        print(json.dumps(results, indent=2))
 
+    def _get_actions(self, domain_objects):
+        #TODO: domain_objects should be indexed by type to prevent
+        #mapping lookups for each oject
+        for obj in domain_objects:
+            type_name = self.ModelType_to_type_name[type(obj)]
+            dict_mapper = self.type_name_to_dict_mapper[type_name]
+            action = dict_mapper.map(obj)
+            action['_index'] = self.index
+            action['_type'] = type_name
+            yield action
+        
     def query(self, ModelType, criteria):
         res = self.es.search(index="iocdb", body={"query": {"match_all": {}}})
         schema = self.ModelType_to_schema_mapper[ModelType]
